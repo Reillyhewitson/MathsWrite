c1aa0b60b4cef1d30bc65d4d359229eafc684eca
Reillyhewitson <reilly.hewitson@utcreading.org.uk>
2018-05-04 09:23:20 +0100

    Commented code

--- MathsWrite.py
+++ MathsWrite.py
@@ -3,11 +3,12 @@ while True:
     try:
         print('Input calculation (Ctrl - C to close)')
         x = input()
-        file = open(os.getcwd()+'\\Maths.txt', 'a+')
-        file.write(str(x))
-        answer = round((eval(x)),2)
-        file.write('='+str(answer)+'\n')
+        file = open(os.getcwd()+'\\Maths.txt', 'a+') #Gets the current directory and opens or creates a file there
+        file.write(str(x)) #writes the calculation put in by the user to the file
+        answer = round((eval(x)),2) #this does rounding as well as doing the calculation by usin pythons eval function
+        file.write('='+str(answer)+'\n') #adds the anwer to the inputted calculation and starts a new line
         print (answer)
         file.close()
     except KeyboardInterrupt:
         break
+
